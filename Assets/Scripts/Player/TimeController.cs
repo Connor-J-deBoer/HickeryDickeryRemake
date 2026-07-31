4efa8c6dc610b153f160a0327dd276c04aa7bf2f
Connor J deBoer <connor.deboer04@gmail.com>
2026-07-30 23:53:38 -0600

    Got time control working so it can be toggled and you have a limited amount of rewind

--- Assets/Scripts/Player/TimeController.cs
+++ Assets/Scripts/Player/TimeController.cs
@@ -1,5 +1,6 @@
 // Copyright © Connor deBoer (MQG) 2026, All Rights Reserved
 
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,25 +9,51 @@ namespace HickeryDickery.Player
     [RequireComponent(typeof(PlayerPositionTracking))]
     public class TimeController : MonoBehaviour
     {
+        [SerializeField] private bool _controlTime = false;
         [SerializeField] private PlayerPositionTracking _positions;
         [SerializeField] private Rigidbody _rigidbody;
-        private float _timeDelta;
+        private float _timeDelta = 1;
         void OnValidate()
         {
             _positions = GetComponent<PlayerPositionTracking>();
             _rigidbody = GetComponent<Rigidbody>();
+            Time.timeScale = 1;
+        }
+        private void Start()
+        {
+            StartCoroutine(CapturePositionInRealTime());
         }
         private void Update()
         {
-            Time.timeScale = Mathf.Clamp(_timeDelta * Time.unscaledDeltaTime, 0, 100);
-            if (_timeDelta > 0)
-                _positions.AddPosition();
-            else if (_timeDelta < 0)
+            Time.timeScale = Mathf.Clamp(_timeDelta, 0, 100);
+            
+            if (_timeDelta < 0)
                 (transform.position, _rigidbody.linearVelocity, _rigidbody.angularVelocity, transform.rotation) = _positions.GetPreviousPosition();
+        }
+
+        private IEnumerator CapturePositionInRealTime()
+        {
+            while (true)
+            {
+
+                if (!_controlTime)
+                    yield return new WaitForSecondsRealtime(0.1f);
+                else
+                    yield return new WaitForEndOfFrame();
+                if (_timeDelta > 0)
+                    _positions.AddPosition();
+            }
         }
         private void OnTimeControl(InputValue value)
         {
+            if (!_controlTime)
+                return;
             _timeDelta = value.Get<float>();
+        }
+        private void OnToggleTimeControl(InputValue _)
+        {
+            _controlTime = !_controlTime;
+            _timeDelta = _controlTime ? 0 : 1;
         }
     }
 }
